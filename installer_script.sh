95c4f6b970dc7cb3feb58cd0b961f58e5dd742b0
m-squared96 <michael.moore34@mail.dcu.ie>
2018-11-27 20:16:04 +0000

    Initial commit

---
+++ installer_script.sh
@@ -0,0 +1,20 @@
+cd $HOME
+
+sudo apt update
+
+sudo apt install vim octave liboctave-dev firefox texstudio texlive texlive-extra-utils libreoffice gnome-mpv tilix virtualbox python3-pip python3-tk jupyter-notebook ipython3 neofetch git
+
+sudo add-apt-repository -y ppa:smathot/cogscinl
+sudo apt update
+sudo apt install zotero-standalone
+
+sudo snap refresh
+sudo snap install vscode --classic
+sudo snap install spotify --classic
+sudo snap install telegram-desktop --classic
+sudo snap install mailspring --classic
+
+sudo -H pip3 install numpy pandas matplotlib scikit-learn scipy astropy seaborn pylint 
+
+sudo apt autoremove
+sudo apt -y upgrade
