a3eb594917337b42248fc443f02a659666eb3613
m-squared96 <michael.moore34@mail.dcu.ie>
2018-11-29 14:09:58 +0000

    Added MyHDL Python Package and vim files

--- installer_script.sh
+++ installer_script.sh
@@ -4,6 +4,9 @@ sudo apt update
 
 sudo apt install vim octave liboctave-dev firefox texstudio texlive texlive-extra-utils libreoffice gnome-mpv tilix virtualbox python3-pip python3-tk jupyter-notebook ipython3 neofetch git gimp youtube-dl
 
+cp -r .vim ~/
+cp .vimrc ~/
+
 sudo add-apt-repository -y ppa:smathot/cogscinl
 sudo apt update
 sudo apt install zotero-standalone
@@ -14,7 +17,7 @@ sudo snap install spotify --classic
 sudo snap install telegram-desktop --classic
 sudo snap install mailspring --classic
 
-sudo -H pip3 install numpy pandas matplotlib scikit-learn scipy astropy seaborn pylint 
+sudo -H pip3 install numpy pandas matplotlib scikit-learn scipy astropy seaborn pylint myhdl 
 
 sudo apt autoremove
 sudo apt -y upgrade
