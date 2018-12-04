fd600ecb00f737dc6dc97834bc6322664427fe47
m-squared96 <michael.moore34@mail.dcu.ie>
2018-12-04 14:54:46 +0000

    Removed Virtualbox install

--- installer_script.sh
+++ installer_script.sh
@@ -2,7 +2,7 @@ cd $HOME
 
 sudo apt update
 
-sudo apt install vim octave liboctave-dev firefox texstudio texlive texlive-extra-utils libreoffice gnome-mpv tilix virtualbox python3-pip python3-tk jupyter-notebook ipython3 neofetch git gimp youtube-dl
+sudo apt install vim octave liboctave-dev firefox texstudio texlive texlive-extra-utils libreoffice gnome-mpv tilix python3-pip python3-tk jupyter-notebook ipython3 neofetch git gimp youtube-dl
 
 cp -r .vim ~/
 cp .vimrc ~/
