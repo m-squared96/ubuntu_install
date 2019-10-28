049f3e4a51175131a402ffcef696aa6fe2fcd004
m-squared96 <michael.moore34@mail.dcu.ie>
2019-10-28 18:32:01 +0000

    Added Python packages and Rust + Curl install

--- installer_script.sh
+++ installer_script.sh
@@ -2,7 +2,7 @@ cd $HOME
 
 sudo apt update
 
-sudo apt install vim firefox gnome-mpv python3-pip python3-tk jupyter-notebook ipython3 neofetch git gimp youtube-dl python-tk python-pip 
+sudo apt install vim firefox gnome-mpv python3-pip python3-tk python3-venv jupyter-notebook ipython3 neofetch git gimp youtube-dl python-tk python-pip curl
 
 cp -r .vim ~/
 cp .vimrc ~/
@@ -15,3 +15,8 @@ sudo snap install telegram-desktop --classic
 sudo apt -y upgrade
 sudo apt autoremove
 sudo ubuntu-drivers autoinstall
+
+git config --global user.email "michael.moore34@mail.dcu.ie"
+git config --global user.name "m-squared96"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
