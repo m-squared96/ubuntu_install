0024b2a3216062873f5fdbfd69d5e7ad04197bfb
m-squared96 <michael.moore34@mail.dcu.ie>
2020-08-23 12:11:41 +0100

    Updated installer script and added bash aliases

---
+++ .bash_aliases
@@ -0,0 +1,9 @@
+alias upd="sudo apt update && sudo apt -y upgrade && sudo apt autoremove && sudo snap refresh"
+
+alias python="python3"
+alias pip="pip3"
+
+alias pdev="cd ~/code/projects/pyplanedev/branches/ && source ../bin/activate && tree" 
+alias dsci="cd ~/code/dsci/src/ && source ../bin/activate && tree"
+
+alias off="deactivate && cd ~"
--- installer_script.sh
+++ installer_script.sh
@@ -1,16 +1,14 @@
-cd $HOME
-
 sudo apt update
 
-sudo apt install vim firefox gnome-mpv python3-pip python3-tk python3-venv jupyter-notebook ipython3 neofetch git gimp youtube-dl python-tk python-pip curl
+sudo apt -y install curl gcc g++ git gnome-multi-writer gzip jupyter latte-dock libreoffice libsdl2-dev make neofetch python3 python3-nacl python3-pip python3-pymacaroons python3-tk python3-venv tree vim yakuake
 
 cp -r .vim ~/
 cp .vimrc ~/
 
 sudo snap refresh
-sudo snap install vscode --classic
-sudo snap install spotify --classic
 sudo snap install telegram-desktop --classic
+sudo snap install ncspot
+sudo snap install pyplane --edge
 
 sudo apt -y upgrade
 sudo apt autoremove
@@ -20,3 +18,33 @@ git config --global user.email "michael.moore34@mail.dcu.ie"
 git config --global user.name "m-squared96"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cd $HOME
+
+sudo pip3 install jupyter
+
+mkdir ~/code
+cd ~/code/
+python3 -m venv dsci
+cd dsci
+source bin/activate
+pip3 install jupyter numpy pandas matplotlib seaborn scikit-learn statsmodels 
+deactivate
+
+cd ../
+mkdir projects/
+cd projects/
+python3 -m venv pyplanedev
+cd pyplanedev
+source bin/activate
+pip3 install numpy matplotlib scipy PyQt5 PyYAML sympy pre-commit
+deactivate
+
+cd ../
+mkdir roguelike/
+git clone https://github.com/m-squared96/Rougelike_Tutorial roguelike/
+
+cd $HOME
+
+mkdir stats/
+mkdir maths/
